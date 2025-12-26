ce10cbf601d7e57e2e2be9199233556ec8dafebc
Eduardo <edu-gabriel@hotmail.com>
2025-12-26 11:18:26 -0500

    Part1

--- part1/courseinfo/test.js
+++ part1/courseinfo/test.js
@@ -1,4 +1,15 @@
-const t = [1, 2, 3]
+const sum = (p1, p2) => {
+    console.log(p1)
+    console.log(p2)
 
-const m2 = t.map(value => '<li>' + value + '</li>')
-console.log(m2)
+    return p1 + p2
+}
+
+const result = sum(1, 5)
+console.log(result)
+
+const square = p => p * p
+
+const t = [1, 2, 3]
+const tSquared = t.map(p => p * p)
+
