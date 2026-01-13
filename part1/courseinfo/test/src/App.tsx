e2b04d69020bc4824a0c1623e3b6f6caef3428c9
Eduardo <edu-gabriel@hotmail.com>
2026-01-13 18:20:34 -0500

    Update App.tsx

--- part1/courseinfo/test/src/App.tsx
+++ part1/courseinfo/test/src/App.tsx
@@ -23,27 +23,19 @@ const Button = ({handleClick, text}) => (
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
-  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
-  }
-
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const setToValue = (newValue) => () =>{
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return(
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' /> 
-      {right}
-      <History allClicks={allClicks} />
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
     </div>
   )
 }
