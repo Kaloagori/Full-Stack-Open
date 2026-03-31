05b43f3f3a9296e95c1f4532744b5e39b8b43324
Eduardo <250883501+Kaloagori@users.noreply.github.com>
2026-03-31 16:55:49 -0500

    update blog

---
+++ part4/blog/controllers/blogs.js
@@ -0,0 +1,24 @@
+import express from 'express'
+const blogRouter = express.Router()
+import Blog from '../models/blog.js'
+
+blogRouter.get('/', (request, response) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
+})
+
+blogRouter.post('/', (request, response, next) => {
+    const blog = new Blog(request.body)
+
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+        .catch(error => next(error))
+})
+
+export default blogRouter
--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,14 +1,17 @@
+import app from "./app.js"
+import config from "./utils/config.js"
+import logger from "./utils/logger.js"
+
+app.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
+})
+
+
+/** 
 import express from 'express'
 const app = express()
 import cors from 'cors'
 import mongoose from 'mongoose'
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
 
 const Blog = mongoose.model('Blog', blogSchema)
 
@@ -18,26 +21,9 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
 const PORT = 3003
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+    */
